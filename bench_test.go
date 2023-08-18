5e262771900a5eeb2114cf97bcbf77d1e4ec06ee
Tyler Yahn <codingalias@gmail.com>
2023-08-17 17:07:40 -0700

    Update bench_test

--- bench_test.go
+++ bench_test.go
@@ -2,9 +2,17 @@ package sampling
 
 import "testing"
 
+var result int64
+
 func BenchmarkSampler(b *testing.B) {
 	const n = 1024
 	const value = 1
+
+	b.Run("BaseRandomInt64", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			result = rng.Int63()
+		}
+	})
 
 	b.Run("R", func(b *testing.B) {
 		sampler := NewR(n)
@@ -22,6 +30,34 @@ func BenchmarkSampler(b *testing.B) {
 
 	b.Run("L", func(b *testing.B) {
 		sampler := NewL(n)
+
+		// Measure random insert, not initial loading.
+		for i := 0; i < n; i++ {
+			sampler.Offer(value)
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			sampler.Offer(value)
+		}
+	})
+
+	b.Run("X", func(b *testing.B) {
+		sampler := NewX(n)
+
+		// Measure random insert, not initial loading.
+		for i := 0; i < n; i++ {
+			sampler.Offer(value)
+		}
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			sampler.Offer(value)
+		}
+	})
+
+	b.Run("Z", func(b *testing.B) {
+		sampler := NewZ(n, 40)
 
 		// Measure random insert, not initial loading.
 		for i := 0; i < n; i++ {
