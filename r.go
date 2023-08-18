04211d88b87cb57e994743d26b7f8b89ad274b6c
Tyler Yahn <codingalias@gmail.com>
2023-08-17 17:07:07 -0700

    Fix algo R

--- r.go
+++ r.go
@@ -8,15 +8,15 @@ import (
 var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 type R struct {
-	reservoir []int64
+	reservoir []float64
 	counter   int64
 }
 
 func NewR(n int) *R {
-	return &R{reservoir: make([]int64, n)}
+	return &R{reservoir: make([]float64, n)}
 }
 
-func (r *R) Offer(value int64) {
+func (r *R) Offer(value float64) {
 	r.counter++
 	if int(r.counter) <= cap(r.reservoir) {
 		r.reservoir[r.counter-1] = value
@@ -27,4 +27,12 @@ func (r *R) Offer(value int64) {
 	if j < cap(r.reservoir) {
 		r.reservoir[j] = value
 	}
+}
+
+func (r *R) mean() float64 {
+	var sum float64
+	for _, v := range r.reservoir {
+		sum += v
+	}
+	return sum / float64(cap(r.reservoir))
 }
